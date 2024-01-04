92e3927ff9ddeece4454fa373f602d4f39f1ea5a
Agie Fadillah <arigierf@gmail.com>
2024-01-04 09:04:47 +0700

    1.0

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import punycode from 'punycode';
+import encrypt from 'mongoose-encryption';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = 'Thisisourlittlesecret.';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model('User', userSchema);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  try {
+    await newUser.save();
+    res.render('secrets');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // User.findOne({ email: username }, function (err, foundUser) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       if (foundUser.password === password) {
+  //         res.render('secrets');
+  //       }
+  //     }
+  //   }
+  // });
+
+  try {
+    const foundUser = await User.findOne({ email: username });
+
+    if (foundUser && foundUser.password === password) {
+      res.render('secrets');
+    } else {
+      res.render('login');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
